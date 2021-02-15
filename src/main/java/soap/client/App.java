e118f19f91c59558783dbbc1f264634ac902dc7f
e.patti <e.patti.guest@posteitaliane.it>
2021-02-15 17:54:57 +0100

    initial commit

---
+++ src/main/java/soap/client/App.java
@@ -0,0 +1,29 @@
+package soap.client;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.consumingwebservice.wsdl.GetCountryResponse;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = quoteClient.getCountry(country);
+			System.err.println(response.getCountry().getCapital());
+		};
+	}
+}
---
+++ src/test/java/soap/client/SoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package soap.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
