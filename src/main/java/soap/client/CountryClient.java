ade14cf8115935ff462c41a880420d23f42240dd
e.patti <e.patti.guest@posteitaliane.it>
2021-02-16 12:00:55 +0100

    latest

--- src/main/java/soap/client/CountryClient.java
+++ src/main/java/soap/client/CountryClient.java
@@ -2,7 +2,6 @@ package soap.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
